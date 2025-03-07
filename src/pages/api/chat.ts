d4a33f4d311f887a0b375c3ae2eea81f52f56d17
Aurélio <venturelli9117@gmail.com>
2025-03-06 21:33:41 -0300

    Início da refatoração das interfaces. Colocadas em um único arquivo; Requisição com a API da OpenIA falhou; Tela de Chat funcional, pegando o usuário, timestamp e mensagem salva por meio de gerenciamento de estados

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(
+export default async function chatApi(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
@@ -9,12 +9,13 @@ export default async function handler(
 	}
 	try {
 		const { message } = req.body;
+		const apiKey = process.env.OPENAI_API_KEY;
 
 		const response = await fetch("https://api.openai.com/v1/chat/completions", {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
-				Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+				Authorization: `Bearer ${apiKey}`,
 			},
 			body: JSON.stringify({
 				model: "gpt-3.5-turbo",
--- src/services/chatService.ts
+++
@@ -1,7 +0,0 @@
-export const getMessages = async (message: string) => {
-	const response = await fetch("/api/chat", {
-		method: "POST",
-		body: JSON.stringify({ message }),
-	});
-	return response.json();
-};
