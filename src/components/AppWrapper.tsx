073f0e1b3b08eb44844945a242fa3512844eac3f
Aurélio Venturelli <venturelli9117@gmail.comgit config --global user.email venturelli9117@gmail.com>
2025-05-03 19:27:32 -0300

    Refatoração: Modo janela funcional.

--- src/components/AppWrapper.tsx
+++ src/components/AppWrapper.tsx
@@ -6,8 +6,8 @@ import MessageLayout from "./MessageLayout";
 const AppWrapper: React.FC = () => {
 	return (
 		<>
-			<div className="flex flex-col h-full">
-				<div className="flex-1 overflow-y-auto px-4 py-4 h-[400px] bg-blue-50/50">
+			<div className="w-[500px] flex flex-col h-full bg-white rounded-lg shadow-lg overflow-hidden">
+				<div className="flex-1 overflow-y-auto px-4 py-4 h-[300px] bg-blue-50/50">
 					<ChatHistory />
 					<MessageLayout />
 				</div>
